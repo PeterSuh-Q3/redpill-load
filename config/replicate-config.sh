4b50b808c580e74e7c77e161f69580fc871bbb66
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-10-28 15:29:52 +0000

    7.3.1-86003

--- config/replicate-config.sh
+++ config/replicate-config.sh
@@ -3,7 +3,7 @@
 for m in `cat platform73`
 do 
 echo "Working on $m"
-cp -rp $m/${1}-${2} $m/${3}-${4}
-sed -i "s/${1}/${2}/g" $m/${3}-${4}/config.json
-sed -i "s/${3}/${4}/g" $m/${3}-${4}/config.json
+#cp -rp $m/${1}-${2} $m/${3}-${4}
+sed -i "s/${1}/${3}/g" $m/${3}-${4}/config.json
+sed -i "s/${2}/${4}/g" $m/${3}-${4}/config.json
 done
