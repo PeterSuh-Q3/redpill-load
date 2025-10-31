274e26262afbaa7d22a093b9bf62dc86398e0c1d
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-10-31 18:51:58 +0900

    Update replicate-config.sh for platform62

--- config/replicate-config.sh
+++ config/replicate-config.sh
@@ -1,9 +1,10 @@
 # example ${1} = "7.3.0", ${2} = "81180", ${3} = "7.3.1", ${4} = "86003"
+# ./replicate-config.sh 7.3.0 81180 6.2.4 25556
 
-for m in `cat platform73`
+for m in `cat platform62`
 do 
 echo "Working on $m"
-#cp -rp $m/${1}-${2} $m/${3}-${4}
+cp -rp $m/${1}-${2} $m/${3}-${4}
 sed -i "s/${1}/${3}/g" $m/${3}-${4}/config.json
 sed -i "s/${2}/${4}/g" $m/${3}-${4}/config.json
 done
